a8be6451326639ab759436aec14b07b61930b057
DemonHuB <57061430+TheDemonHuB@users.noreply.github.com>
2020-03-18 13:06:15 +0530

    Update carbon.py

--- userbot/plugins/carbon.py
+++ userbot/plugins/carbon.py
@@ -62,7 +62,7 @@ async def carbon_api(e):
    await e.client.send_file(
          e.chat_id,
          file,
-         caption="Carbon by [@viperadnan](https://t.me/viperadnan)",
+         caption="Carbon by [@thedemonhub](https://t.me/thedemonhub)",
          force_document=True,
          reply_to=e.message.reply_to_msg_id,
          )
